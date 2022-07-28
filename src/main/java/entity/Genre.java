8e739d9027311d8377ddb0af8220ba3e4df60a60
timotheehrl <tharleaux@gmail.com>
2022-07-28 10:26:55 +0200

    entity genre v1

---
+++ src/main/java/entity/Genre.java
@@ -0,0 +1,61 @@
+package entity;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+
+@Entity
+@Table(name = "genres")
+@XmlRootElement(name = "genre")
+public class Genre {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Genre() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Genre)) return false;
+        Genre genre = (Genre) o;
+        return getId() == genre.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
