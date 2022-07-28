d96089f29c7d8e2d61698d8c66c0eca95eab3502
lvlrs4114 <benjamin.lapijover@hotmail.fr>
2022-07-28 14:53:23 +0200

    entity_full

--- src/main/java/com/mycompany/entity/Actor.java
+++ src/main/java/com/mycompany/entity/Actor.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import com.mycompany.entity.Movie;
 
-@XmlRootElement(name = "actors")
+@XmlRootElement(name = "actor")
 @Entity
 @Table(name = "actors")
 public class Actor {
@@ -19,7 +19,7 @@ public class Actor {
     @Id
     @Column(name = "id_actor")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int idActor;
 
     @Column(length = 100)
     private String firstname;
@@ -30,29 +30,29 @@ public class Actor {
     @Column(length = 100)
     private String birthDate;
 
-    @Column(name="movies")
-    @ManyToMany(fetch = FetchType.LAZY ) 
+    @Column(name = "movies")
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "actors_movie",
-                joinColumns = @JoinColumn( name = "id_actor" ),
-                inverseJoinColumns = @JoinColumn( name = "id_movie" ) )
+            joinColumns = @JoinColumn(name = "id_actor"),
+            inverseJoinColumns = @JoinColumn(name = "id_movie"))
     private List<Movie> Movies;
 
     public Actor() {
     }
 
-    public Actor( String firstname, String lastname, String birthDate, List<Movie> Movies) {
+    public Actor(String firstname, String lastname, String birthDate, List<Movie> Movies) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.birthDate = birthDate;
         this.Movies = Movies;
     }
 
-    public int getId() {
-        return id;
+    public int getIdActor() {
+        return idActor;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdActor(int idActor) {
+        this.idActor = idActor;
     }
 
     public String getFirstname() {
@@ -87,6 +87,18 @@ public class Actor {
         this.Movies = Movies;
     }
 
-    
-    
+   public void copy(Actor data) {
+        if (data.getFirstname() != null) {
+            this.firstname = data.getFirstname();
+        }
+        if (data.getLastname() != null) {
+            this.lastname = data.getLastname();
+        }
+        if (data.getBirthDate() != null) {
+           this.birthDate = data.getBirthDate();
+        }
+        if (data.getMovies() != null) {
+           this.Movies = data.getMovies();
+        }
+    }
 }
